d14890e9b7988307a41202748e3114d811165984
yassineboucham <yassineboucham11@gamil.com>
2023-11-17 05:10:29 +0000

    add continue and free if isatty

--- shell.c
+++ shell.c
@@ -28,7 +28,11 @@ int main(int argc, char **argv)
 		free(input), exit(0);
 	if (execute_cmd(input, argv[0]))
 		break;
+
+	else
+		continue;
 	}
+	if (isatty(STDIN_FILENO))
 	free(input);
 	return (0);
 }
